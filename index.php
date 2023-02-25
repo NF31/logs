bd391ec86df41bdd3cdaf45d3e0abace27f49011
Jean-Eudes Woto <107761073+NF31@users.noreply.github.com>
2023-02-25 05:20:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <h1>Index zone</h1>
+    <br>
+    <a href="page1.php">Welcome to the level 1</a>
+    <br>
+    <a href="page2.php">Welcome to the level 2</a>
+    <br>
+    <a href="page3.php">Welcome to the level 3</a>
+    <br>
+    <br>
+    <a href="index.php">Index</a>
+    <br>
+    <?php
+
+
+    $month = "[" . date("d") . "/" . date("m") . "/" . date("y") . "]";
+    $hour = "[" . date("H") . ":" . date("i") . ":" . date("s") . "]";
+    $url = $_SERVER['REMOTE_ADDR'] . " connect to " . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+    $reunion = $month . $hour . $url . "\n";
+    echo $reunion;
+
+    $files = fopen("logs.txt", "a+");
+    fputs($files, $reunion);
+    fclose($files);
+
+    ?>
+
+</body>
+
+</html>
---
+++ page2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> SS 2</h1>
+    <br>
+<a href="page1.php">SS 1</a>
+<br>
+<a href="page2.php">SS 2</a>
+<br>
+<a href="page3.php">SS 3</a>
+<br>
+<br>
+<a href="index.php">Index</a>
+
+<?php
+
+$month = "[".date("d")."/".date("m")."/".date("y")."]";
+$hour = "[".date("H").":".date("i").":".date("s")."]";
+$url = $_SERVER['REMOTE_ADDR']." connect to " .$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
+$reunion = $month.$hour.$url."\n";
+echo $reunion;
+
+$files = fopen("logs.txt", "a+");
+fputs($files,$reunion);
+fclose($files);
+
+?>
+</body>
+</html>
